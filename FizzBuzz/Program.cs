0057993ea1e5955d7674f43b48e72ea87908715a
Alejandro Jaramillo <ajjaramillo7@gmail.com>
2024-10-01 11:24:10 -0600

    Create Sample Programs

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿/* if i % 3 == 0 then (Fizz)
+	if i % 5 == 0 then (Buzz)
+*/
+
+
+// namespace is like a 'Folder' that organizes your files
+namespace FizzBuzz;
+
+class Program{
+    // Entry Point
+    static void Main(string[] args){
+
+        Console.WriteLine("Hello World");
+
+    }
+
+		// public (access to all classes)
+		// private (restricted to only the same class declared in)
+		// protected (accessed to itself and subclasses only)
+
+		public static void FizzBuzz(int n){
+
+			for (int i = 1; i < n; i++){
+
+				if (i % 15 == 0){
+					Console.WriteLine("FizzBuzz");
+				}
+				else if (i % 5 == 0){
+					Console.WriteLine("Buzz");
+				}
+				else if (i % 3 == 0){
+					Console.WriteLine("Fizz");
+				}
+				else{
+					Console.WriteLine(i);
+				}
+			}
+
+		}
+}
